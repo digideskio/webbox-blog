45068abedc387767510904d7b271203842676bf6
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2014-06-30 21:53:13 +0300

    share icons changed

--- _plugins/fontawesome.rb
+++ _plugins/fontawesome.rb
@@ -11,4 +11,18 @@ module Jekyll
   end
 end
 
+module Jekyll
+  class FontawesomeCircleTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @icon = text
+    end
+
+    def render(context)
+      "<span class=\"fa-stack fa-lg\"><i class=\"fa fa-circle fa-stack-2x\"></i><i class=\"fa fa-#{@icon} fa-stack-1x fa-inverse\"></i></span>"
+    end
+  end
+end
+
 Liquid::Template.register_tag('fa_icon', Jekyll::FontawesomeTag)
+Liquid::Template.register_tag('fa_icon_circle', Jekyll::FontawesomeCircleTag)
